c840891d68bafe4a4658d32a4036738e49de3cc4
Gantumur <tsgantumur@gmail.com>
2019-10-03 09:12:15 -0500

    Commit Lab03

---
+++ Lab03/Calculator/src/main/java/mum/edu/controller/CalculatorController.java
@@ -0,0 +1,75 @@
+package mum.edu.controller;
+
+import mum.edu.domain.Calculation;
+import mum.edu.validator.CalcValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@RequestMapping("/")
+@Controller
+public class CalculatorController {
+	private static final long serialVersionUID = 1L;
+
+	@Autowired
+	private CalcValidator productValidator;
+
+	@RequestMapping(value = { "/", "/calculator.do" } )
+	public String calculate(HttpServletRequest request, HttpServletResponse response) {
+
+		return "calculator";
+	}
+
+	@RequestMapping(value = "/result.do")
+	public String result(Calculation calc, HttpServletRequest request, HttpServletResponse response) {
+
+		calc.setAdd1(request.getParameter("add1").trim());
+		calc.setAdd2(request.getParameter("add2").trim());
+		calc.setMult1(request.getParameter("mult1").trim());
+		calc.setMult2(request.getParameter("mult2").trim());
+		String sum = "";
+		String mult = "";
+
+		List<String> errors = productValidator.validate(calc);
+
+		if(errors.isEmpty())
+		{
+			try {
+				Integer a1 = Integer.parseInt(calc.getAdd1());
+				Integer a2 = Integer.parseInt(calc.getAdd2());
+				sum = "" + (a1+a2);
+			} catch(NumberFormatException e) {
+				//do nothing
+			}
+
+			try {
+				Integer m1 = Integer.parseInt(calc.getMult1());
+				Integer m2 = Integer.parseInt(calc.getMult2());
+				mult = "" + (m1 * m2);
+			} catch(NumberFormatException e) {
+				//do nothing
+			}
+
+			request.setAttribute("add1", calc.getAdd1());
+			request.setAttribute("add2", calc.getAdd2());
+			request.setAttribute("mult1", calc.getMult1());
+			request.setAttribute("mult2", calc.getMult2());
+			request.setAttribute("sum", sum);
+			request.setAttribute("mult", mult);
+
+			return "result";
+		} else {
+			request.setAttribute("errors", errors);
+
+			return "error";
+		}
+
+
+		}
+ 
+
+}
---
+++ Lab03/Calculator/src/main/java/mum/edu/validator/CalcValidator.java
@@ -0,0 +1,8 @@
+package mum.edu.validator;
+
+import java.util.List;
+
+public interface CalcValidator {
+	
+	public List<String> validate(Object object) ;
+}
